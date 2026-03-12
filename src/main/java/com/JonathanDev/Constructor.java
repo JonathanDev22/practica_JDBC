98861542c6ee853103bf78253aa1a58eaafea89e
Jonathan Carpio <jcarpio@swarmingts.com>
2026-03-12 10:02:14 +0100

    Refactor main method to utilize Product record for displaying product details

--- src/main/java/com/JonathanDev/Constructor.java
+++ src/main/java/com/JonathanDev/Constructor.java
@@ -2,11 +2,12 @@ package com.JonathanDev;
 
 import com.JonathanDev.Utils.ConnectionBD;
 import com.JonathanDev.Utils.QuerysBD;
+import com.JonathanDev.model.Product;
 
 import java.sql.*;
 
 public class Constructor {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         int contador = 0;
         System.out.println("Inicio de aplicacion usando JDBC");
         try (Statement stmt = ConnectionBD.getConnection().createStatement()) {
@@ -14,14 +15,15 @@ public class Constructor {
             stmt.executeQuery(QuerysBD.SELECT_ALL_PRODUCTS);
             try (ResultSet rs = stmt.getResultSet()) {
                 while (rs.next()) {
+                    Product p = new Product(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getInt("price"),
+                            rs.getDate("date_create")
+                    );
+                    System.out.println(p);
                     contador++;
-                    System.out.print("ID : " + rs.getInt("id"));
-                    System.out.print(" | ");
-                    System.out.print("Nombre : " + rs.getString("name"));
-                    System.out.print(" | ");
-                    System.out.print("Precio : " + rs.getDouble("price"));
-                    System.out.print(" | ");
-                    System.out.println("Date : " + rs.getDate("date_create"));
+
                 }
                 if (contador == 0) {
                     System.out.println("####### No se ha encontrado ningun registro ######");
