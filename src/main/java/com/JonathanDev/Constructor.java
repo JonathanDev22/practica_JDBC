ba12072ae608b5db8e3a8b45d89c792189c0b954
Jonathan Carpio <jcarpio@swarmingts.com>
2026-03-12 09:22:11 +0100

    Add ConnectionBD class for database connection management and update Constructor to use it

--- src/main/java/com/JonathanDev/Constructor.java
+++ src/main/java/com/JonathanDev/Constructor.java
@@ -1,41 +1,35 @@
-        package com.JonathanDev;
+package com.JonathanDev;
 
-        import java.sql.*;
+import com.JonathanDev.Utils.ConnectionBD;
+import com.JonathanDev.Utils.QuerysBD;
 
-        public class Constructor {
-            public static void main(String[] args) {
-                String URL = "jdbc:postgresql://localhost:5432/repo_local";
-                String USER = "admin";
-                String PASSWORD = "root";
-                int contador = 0;
-                String sql = "SELECT * FROM products";
-                //String insert = "INSERT INTO products (name, price, date_create) VALUES ('Producto de prueba', 9.99 , CURRENT_DATE)";
-                System.out.println("Inicio de aplicacion usando JDBC");
-                try {
-                    Connection conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-                    Statement stmt = conexion.createStatement();
-                  //  stmt.executeUpdate(insert);
-                    stmt.execute(sql);
-                    ResultSet rs = stmt.getResultSet();
-                    while (rs.next()){
-                        contador++;
-                        System.out.print("ID : " + rs.getInt("id"));
-                        System.out.print(" | ");
-                        System.out.print("Nombre : " + rs.getString("name"));
-                        System.out.print(" | ");
-                        System.out.print("Precio : " + rs.getDouble("price"));
-                        System.out.print(" | ");
-                        System.out.println("Date : " + rs.getDate("date_create"));
-                    }
-                    if (contador == 0 ){
-                        System.out.println("####### No se ha encontrado ningun registro ######");
-                    }
-                    System.out.println("####### Resultados encontrados: " + contador + " #######");
+import java.sql.*;
 
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    System.out.println("Fin de aplicacion usando JDBC");
+public class Constructor {
+    public static void main(String[] args) {
+        int contador = 0;
+        System.out.println("Inicio de aplicacion usando JDBC");
+        try (Statement stmt = ConnectionBD.getConnection().createStatement()) {
+            stmt.executeUpdate(QuerysBD.INSERT_PRODUCT);
+            stmt.executeQuery(QuerysBD.SELECT_ALL_PRODUCTS);
+            try (ResultSet rs = stmt.getResultSet()) {
+                while (rs.next()) {
+                    contador++;
+                    System.out.print("ID : " + rs.getInt("id"));
+                    System.out.print(" | ");
+                    System.out.print("Nombre : " + rs.getString("name"));
+                    System.out.print(" | ");
+                    System.out.print("Precio : " + rs.getDouble("price"));
+                    System.out.print(" | ");
+                    System.out.println("Date : " + rs.getDate("date_create"));
                 }
+                if (contador == 0) {
+                    System.out.println("####### No se ha encontrado ningun registro ######");
+                }
+                System.out.println("####### Resultados encontrados: " + contador + " #######");
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+    }
+}
